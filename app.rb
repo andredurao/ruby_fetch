409486857a25232da4e1d619cece8e248c9ec25e
Andre Durao <andreluisdurao@gmail.com>
2023-11-03 20:11:18 +0900

    Split app and new service fetcher

--- app.rb
+++ app.rb
@@ -1,32 +1,6 @@
-# frozen_string_literal: true
-
-# Fetcher class: responsible for requests
-class Fetcher
-  require 'uri'
-  require 'net/http'
-
-  attr_reader :uri
-
-  def initialize(url)
-    url = "http://#{url}" unless url.start_with?(/https?:\/\//)
-    @uri = URI(url)
-
-    raise 'Invalid URL' unless uri.host
-  end
-
-  def fetch
-    request = Net::HTTP.get(uri, { 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0' })
-    response = Net::HTTP.start(url.host, url.port, use_ssl: true) { |http| http.request(request) }
-    case response
-    when Net::HTTPSuccess     then response
-    when Net::HTTPRedirection then fetch(response['location'], limit - 1)
-    else
-      response.error!
-    end
-    p response
-  end
-end
+require_relative './fetcher'
 
 ARGV.each do |url|
-  Fetcher.new(url).fetch
+  x = Fetcher.new(url).fetch
+  p x
 end
---
+++ fetcher.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# Fetcher: perform get requests and store the reponse in html file
+class Fetcher
+  require 'uri'
+  require 'net/http'
+  require 'logger'
+
+  attr_reader :url, :logger
+
+  def initialize(url)
+    @url = url.start_with?(/https?:\/\//) ? url : "http://#{url}"
+    @logger = Logger.new(STDOUT)
+  end
+
+  # https://docs.ruby-lang.org/en/master/Net/HTTP.html#class-Net::HTTP-label-Following+Redirection
+  def fetch(limit = 10)
+    raise 'Too many HTTP redirects' if limit.zero?
+    raise 'Invalid URL' unless valid_url?
+
+    res = Net::HTTP.get_response(URI(@url))
+    case res
+    when Net::HTTPSuccess
+      res
+    when Net::HTTPRedirection
+      @url = res['Location']
+      logger.warn("Redirected to #{@url}")
+      fetch(limit - 1)
+    else
+      res.value
+    end
+  end
+
+  private
+
+  def valid_url?
+    !!URI(@url).host
+  end
+
+  def use_ssl?
+    @uri.scheme == 'https'
+  end
+end
